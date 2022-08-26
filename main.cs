ddf72b9dfccbcd12e4219f264bc4cc3a8baa545f
Lucas Marques <105999437+Lucas-MaDeOliveira@users.noreply.github.com>
2022-08-26 00:24:12 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    for(int i=1; i<=50;i++)
+    {
+      if(i%2==1)
+      {
+        Console.WriteLine (i);
+      }
+    }
+    
+  }
+}
